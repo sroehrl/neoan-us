94c1f2b8b548d5f9d1158926b83724516cba5a90
sroehrl <sroehrl@eagleproductivity.com>
2019-12-12 20:12:26 -0500

    update design

--- component/home/home.ctrl.js
+++ component/home/home.ctrl.js
@@ -1,2 +1,26 @@
 
 ScrollReveal().reveal('.reveal',{delay:500});
+
+/*tab card*/
+const tabSystem = {
+    init(){
+        document.querySelectorAll('.tabs-menu').forEach(tabMenu => {
+            Array.from(tabMenu.children).forEach((child, ind) => {
+                child.addEventListener('click', () => {
+                    tabSystem.toggle(child.dataset.target);
+                });
+                if(child.className.includes('is-active')){
+                    tabSystem.toggle(child.dataset.target);
+                }
+            });
+        });
+    },
+    toggle(targetId){
+        console.log(targetId);
+        document.querySelectorAll('.tabs-content').forEach(contentElement=>{
+            contentElement.style.display = contentElement.id === targetId ? 'block' : 'none';
+            document.querySelector(`[data-target="${contentElement.id}"]`).classList[contentElement.id === targetId ? 'add' : 'remove']('is-active');
+        })
+    },
+};
+tabSystem.init();
